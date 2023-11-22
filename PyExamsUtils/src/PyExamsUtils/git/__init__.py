1d0b2e1a8fbec2c71ac44834764ce1b43f5ad977
Marcos Garcia <marcos.garcia@urjc.es>
2023-11-22 20:42:05 +0100

    pyexams renamed

--- PyExamsUtils/src/PyExamsUtils/git/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from ._git import (getRepoName, addTokenToURL, 
-                   getTokensFromGDrive, addTokenFromGDriveToURL, 
-                   pipInstallRepo, cloneRepo, setAccessURL, setRemoteURL, removeRemote, 
-                   download_git_file, download_git_folder)
